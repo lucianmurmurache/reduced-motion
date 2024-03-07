2c65e2dd2229316cb972730b961d96373be831bc
lucianmurmurache <lucianmurmurache@gmail.com>
2024-03-07 22:01:09 +0100

    chore: add js file as placeholder

---
+++ index.js
@@ -0,0 +1,2 @@
+// This file serves as a placeholder for the npm package and does not contain any functional code.
+// Please refer to the package documentation for details on how to use this package.
